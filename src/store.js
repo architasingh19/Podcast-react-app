900bc1faa43696f193f0cb0346bb9afc247362ae
unknown <architasinghcse@gmail.com>
2023-09-21 22:15:54 +0530

    first commit

---
+++ src/store.js
@@ -0,0 +1,14 @@
+import { configureStore } from "@reduxjs/toolkit";
+import userReducer from './slices/userSlice';
+//import podcastReducer from './slices/podcastSlice';
+
+const store = configureStore({
+    reducer:{
+      user:userReducer,
+      //podcasts:podcastReducer,
+
+    }
+});
+
+
+export default store;
